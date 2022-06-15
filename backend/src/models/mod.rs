05726dd1315f5ef140790e9616086863efd3fe20
David Alasow <davidalasow@gmail.com>
2022-06-15 08:32:20 +0200

    Clean up impl. Add new name graphql input.

--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -2,23 +2,33 @@ pub mod artist;
 pub mod release;
 pub mod song;
 
+use std::error::Error;
+
 use juniper::{GraphQLEnum, GraphQLObject};
+use sea_query::enum_def;
 
+#[derive(Clone, Debug, juniper::GraphQLInputObject)]
+pub struct NewName {
+    pub native: String,
+    pub romanized: String,
+    pub english: String,
+}
 
-#[derive(GraphQLObject, Clone, Debug)]
+#[derive(GraphQLObject, Clone, Debug, sqlx::Encode)]
+#[enum_def]
 pub struct Name {
     /// Native name the original variant uses.
     ///
     /// "残酷な天使のテーゼ"
-    native: String,
+    pub native: String,
     /// Romanized variant of the native title.
     ///
     /// "Zankoku na Tenshi no Tēze"
-    romanized: String,
+    pub romanized: String,
     /// English translated name.
     ///
     /// "The Cruel Angel's Thesis"
-    english: String,
+    pub english: String,
 }
 
 #[derive(GraphQLEnum, Clone, Debug)]
@@ -34,30 +44,24 @@ pub struct ExternalSites {
     url: String,
 }
 
-#[automatically_derived]
-    impl<'r> ::sqlx::decode::Decode<'r, ::sqlx::Postgres> for Name {
-        fn decode(
-            value: ::sqlx::postgres::PgValueRef<'r>,
-        ) -> ::std::result::Result<
-            Self,
-            ::std::boxed::Box<
-                dyn ::std::error::Error + 'static + ::std::marker::Send + ::std::marker::Sync,
-            >,
-        > {
-            let mut decoder = ::sqlx::postgres::types::PgRecordDecoder::new(value)?;
-            let native = decoder.try_decode::<String>()?;
-            let romanized = decoder.try_decode::<String>()?;
-            let english = decoder.try_decode::<String>()?;
-            ::std::result::Result::Ok(Name {
-                native,
-                romanized,
-                english,
-            })
-        }
+impl<'r> sqlx::decode::Decode<'r, sqlx::Postgres> for Name {
+    fn decode(
+        value: sqlx::postgres::PgValueRef<'r>,
+    ) -> Result<Self, Box<dyn Error + 'static + Send + Sync>> {
+        let mut decoder = sqlx::postgres::types::PgRecordDecoder::new(value)?;
+        let native = decoder.try_decode::<String>()?;
+        let romanized = decoder.try_decode::<String>()?;
+        let english = decoder.try_decode::<String>()?;
+        Ok(Name {
+            native,
+            romanized,
+            english,
+        })
     }
-    #[automatically_derived]
-    impl ::sqlx::Type<::sqlx::Postgres> for Name {
-        fn type_info() -> ::sqlx::postgres::PgTypeInfo {
-            ::sqlx::postgres::PgTypeInfo::with_name("localized_name")
-        }
+}
+
+impl sqlx::Type<sqlx::Postgres> for Name {
+    fn type_info() -> sqlx::postgres::PgTypeInfo {
+        sqlx::postgres::PgTypeInfo::with_name("localized_name")
     }
+}
